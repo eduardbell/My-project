ad706b91a30c727b06156f15b2c5b79bf9a1ccbd
eduardbell <eduardbell95@gmail.com>
2022-05-30 21:29:36 +0300

    Мой первый проект

--- java.js
+++ java.js
@@ -1,1 +1,18 @@
-const dfgdfg = prompt("Как тебя зовут???", "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцение его????', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцение его????', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
